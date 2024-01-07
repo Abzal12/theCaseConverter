bf231b64bb7b10fbfaaec56c6414aedf57d293a5
hyper <hyperskill@hyperskill.org>
2024-01-08 00:55:10 +0600

    Saving the html-text values to the txt file using JS

--- main.js
+++ main.js
@@ -27,3 +27,22 @@ document.getElementById("sentence-case").addEventListener("click", function () {
     let str = sentences.join(". ");
     document.querySelector("textarea").value = str;
 })
+
+document.getElementById("save-text-file").addEventListener("click", function () {
+    let text = document.getElementById("text-area").value;
+    let fileName = "text.txt";
+    download(fileName, text);
+})
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
